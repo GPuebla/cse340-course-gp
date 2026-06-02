f636a87752566262281614f394885872c2f94145
GPUEBLA <gabrielpuebla16gmail.com>
2026-06-02 12:10:08 -0300

    development -mode_env

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to log all incoming requests
 app.use((req, res, next) => {
-    if (NODE_ENV === 'production') {
+    if (NODE_ENV === 'development') {
         console.log(`${req.method} ${req.url}`);
     }
     next(); // Pass control to the next middleware or route
