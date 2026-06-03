f112411e0c52230913493464434adc9581e170dc
GPUEBLA <gabrielpuebla16gmail.com>
2026-06-03 14:48:48 -0300

    feat: implement organization and project data models and create organization list view

--- src/models/projects.js
+++ src/models/projects.js
@@ -26,7 +26,7 @@ const getProjectsByOrganizationId = async (organizationId) => {
           description,
           location,
           date
-        FROM project
+        FROM projects
         WHERE organization_id = $1
         ORDER BY date;
       `;
