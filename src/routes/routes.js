da815471710529d5c1071a853dc4faea6b10a22c
GPUEBLA <gabrielpuebla16gmail.com>
2026-06-05 16:12:08 -0300

    feat: implement database models for organizations and projects data retrieval

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { showHomePage } from '../controllers/index.js';
-import { showOrganizationsPage , showOrganizationDetailsPage} from '../controllers/organizations.js';
+import { showOrganizationsPage , showOrganizationDetailsPage, showNewOrganizationForm} from '../controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from '../controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from '../controllers/categories.js';
 import { testErrorPage } from '../controllers/errors.js';
@@ -11,10 +11,15 @@ const router = express.Router();
 router.get('/', showHomePage);
 router.get('/organizations', showOrganizationsPage);
 router.get('/organizations/:id', showOrganizationDetailsPage);
+// Route for new organization page
+router.get('/new-organization', showNewOrganizationForm);
+
+
 router.get('/projects', showProjectsPage);
 router.get('/project/:id', showProjectDetailsPage);
 router.get('/categories', showCategoriesPage);
 router.get('/category/:id', showCategoryDetailsPage);
+
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
