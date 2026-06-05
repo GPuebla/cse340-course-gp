aace5c041d043853c3a0c0c90339088055dbdd73
GPUEBLA <gabrielpuebla16gmail.com>
2026-06-05 16:43:32 -0300

    feat: implement organization management module with CRUD routes and database integration

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { showHomePage } from '../controllers/index.js';
-import { showOrganizationsPage , showOrganizationDetailsPage, showNewOrganizationForm} from '../controllers/organizations.js';
+import { showOrganizationsPage , showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm} from '../controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from '../controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from '../controllers/categories.js';
 import { testErrorPage } from '../controllers/errors.js';
@@ -13,6 +13,8 @@ router.get('/organizations', showOrganizationsPage);
 router.get('/organizations/:id', showOrganizationDetailsPage);
 // Route for new organization page
 router.get('/new-organization', showNewOrganizationForm);
+// Route to handle new organization form submission
+router.post('/new-organization', processNewOrganizationForm);
 
 
 router.get('/projects', showProjectsPage);
