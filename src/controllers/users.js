9f958a1ddc27beb8424ad5c03e93c2901f291863
GPUEBLA <gabrielpuebla16gmail.com>
2026-06-16 15:24:20 -0300

    feat: add getAllUsers function import to user controller and adjust dashboard footer placement

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import { createUser, authenticateUser } from '../models/users.js';
+import { createUser, authenticateUser, getAllUsers } from '../models/users.js';
 
 const showUserRegistrationForm = (req, res) => {
     res.render('register', { title: 'Register' });
